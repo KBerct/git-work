95df872fa49dad75a69092e34152fb02cf4136b8
KBerct <121994082+KBerct@users.noreply.github.com>
2023-01-05 17:31:37 +0800

    Update GitTest.java

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix first");
         System.out.println("push test");
         System.out.println("pull test");
+         System.out.println("666 test");
     }
 }
